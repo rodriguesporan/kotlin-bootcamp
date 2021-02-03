7dde36b9a92f7a885d4b6bd93cc4578c7217b480
rodriguesporan <rodriguesporan@gmail.com>
2021-02-03 20:18:56 -0300

    feat: trying differente ways to swich with 'when'

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,37 @@
-fun main() {
-    println("What's your name?")
-    val name= readLine()
-    println("Hello $name!")
+import java.util.*
+
+fun main(args: Array<String>) {
+    var fortune: String
+    for (i in 1..10) {
+        fortune = getFortune(getBirthday())
+        println("Your fortune is: ${fortune}")
+        if (fortune.contains("Take it easy")) break
+    }
+}
+
+fun getFortune(birthday: Int): String {
+    val fortunes = listOf(
+        "You will have a great day!",
+        "Things will go well for you today.",
+        "Enjoy a wonderful day of success.",
+        "Be humble and all will turn out well.",
+        "Today is a good day for exercising restraint.",
+        "Take it easy and enjoy life!",
+        "Treasure your friends because they are your greatest fortune.",
+    )
+
+    val index = when (birthday) {
+        in 0..7 -> 4
+        27, 31 -> 2
+        else -> birthday.rem(fortunes.size)
+    }
+
+    return fortunes[index]
+}
+
+fun getBirthday(): Int {
+    println("Enter your birthday: ")
+    var birthday = readLine()
+    birthday = "27"
+    return birthday?.toIntOrNull() ?: 1
 }
